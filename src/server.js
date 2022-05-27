6d9030da25b833bd245572466fb1fd9a56567f80
Nicolas de Chevigné <nico2che@gmail.com>
2022-05-27 12:07:19 -0400

    Log warn level on prod

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 const app = require("./app.js");
 
+const prod = process.env.NODE_ENV === "production";
 const fastify = app({
   logger: {
-    level: "info",
-    prettyPrint: true,
+    level: prod ? "warn" : "info",
+    prettyPrint: !prod,
   },
 });
 
 const port = process.env.PORT || 3000;
-fastify.listen(port, function (err, address) {
+fastify.listen(port, function (err) {
   console.log(`Listening on ${port}`);
   if (err) {
     fastify.log.error(err);
