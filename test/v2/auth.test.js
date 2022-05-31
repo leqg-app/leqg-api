719f2f88eeb3d38e1e1b9c1456bd53e89872afd6
Nicolas de Chevigné <nico2che@gmail.com>
2022-05-31 19:58:47 -0400

    Add v2 auth & users routes tests

---
+++ test/v2/auth.test.js
@@ -0,0 +1,236 @@
+const tap = require("tap");
+
+const build = require("../mocks/build.js");
+
+const fastify = build();
+tap.teardown(() => fastify.close());
+
+tap.test("Login", async (t) => {
+  t.test("Success", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "admin", password: "azerty" },
+    });
+
+    t.equal(response.statusCode, 200);
+    t.type(response.json().jwt, "string");
+    t.type(response.json().username, "string");
+    t.type(response.json().email, "string");
+    t.type(response.json().favorites, "object");
+    t.type(response.json().contributions, "object");
+  });
+
+  t.test("Miss field", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "user" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Wrong username", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "wronguser", password: "azerty" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Wrong password", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "admin", password: "wrongpassword" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Blocked user", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "blocked", password: "azerty" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Wrong provder", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "google", password: "azerty" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+});
+
+tap.test("Register", async (t) => {
+  t.test("Success", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local/register",
+      payload: { username: "user", password: "azerty", email: "user@leqg.app" },
+    });
+
+    t.equal(response.statusCode, 200);
+  });
+
+  t.test("Miss field", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local/register",
+      payload: {
+        username: "user",
+        password: "azerty",
+      },
+    });
+
+    console.log(response.json());
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Already exist username", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local/register",
+      payload: {
+        username: "user",
+        password: "azerty",
+        email: "anotheruser@leqg.app",
+      },
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Already exist email", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local/register",
+      payload: {
+        username: "anotheruser",
+        password: "azerty",
+        email: "user@leqg.app",
+      },
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+});
+
+tap.test("Forgot password", async (t) => {
+  t.test("Success", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/forgot-password",
+      payload: { email: "admin@leqg.app" },
+    });
+
+    t.equal(response.statusCode, 200);
+  });
+
+  t.test("Miss field", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/forgot-password",
+      payload: {},
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("User doesn't exist", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/forgot-password",
+      payload: { email: "wrongmail@leqg.app" },
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Blocked user", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/forgot-password",
+      payload: { email: "blocked@leqg.app" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Wrong provder", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/forgot-password",
+      payload: { email: "google@leqg.app" },
+    });
+    t.equal(response.statusCode, 400);
+  });
+});
+
+tap.test("Reset password", async (t) => {
+  t.test("Miss field", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/reset-password",
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Password mismatch", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/reset-password",
+      payload: {
+        code: "reset-code",
+        password: "password",
+        passwordConfirmation: "mismatch",
+      },
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Expired reset code", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/reset-password",
+      payload: {
+        code: "expired-reset-code",
+        password: "password",
+        passwordConfirmation: "password",
+      },
+    });
+
+    t.equal(response.statusCode, 400);
+  });
+
+  t.test("Success", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/reset-password",
+      payload: {
+        code: "reset-code",
+        password: "password",
+        passwordConfirmation: "password",
+      },
+    });
+
+    t.equal(response.statusCode, 200);
+  });
+
+  t.test("Could login with new password", async (t) => {
+    const response = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "admin", password: "password" },
+    });
+
+    t.equal(response.statusCode, 200);
+  });
+});
---
+++ test/v2/user.test.js
@@ -0,0 +1,84 @@
+const tap = require("tap");
+const { createSigner } = require("fast-jwt");
+const signSync = createSigner({ key: process.env.JWT_SECRET });
+
+const build = require("../mocks/build.js");
+
+const fastify = build();
+tap.teardown(() => fastify.close());
+
+tap.test("Profile", async ({ context }) => {
+  tap.test("Login", async (t) => {
+    const login = await fastify.inject({
+      method: "POST",
+      url: "/v2/auth/local",
+      payload: { identifier: "admin", password: "azerty" },
+    });
+    t.equal(login.statusCode, 200);
+
+    const { jwt } = login.json();
+    t.type(jwt, "string");
+    context.jwt = jwt;
+  });
+
+  tap.test("Get user profile", async (t) => {
+    const { jwt } = context;
+
+    const profile = await fastify.inject({
+      url: "/v2/users/me",
+      headers: {
+        authorization: `Bearer ${jwt}`,
+      },
+    });
+
+    t.equal(profile.statusCode, 200);
+    t.equal(profile.json().username, "admin");
+  });
+
+  tap.test("Invalid jwt", async (t) => {
+    const profile = await fastify.inject({
+      url: "/v2/users/me",
+      headers: {
+        authorization: `Bearer invalidJwt`,
+      },
+    });
+
+    t.equal(profile.statusCode, 401);
+  });
+
+  tap.test("Blocked jwt", async (t) => {
+    const profile = await fastify.inject({
+      url: "/v2/users/me",
+      headers: {
+        // valid jwt with blocked user
+        authorization: `Bearer ${signSync({ id: 3 })}`,
+      },
+    });
+
+    t.equal(profile.statusCode, 403);
+  });
+
+  tap.test("Add favorite store to user", async (t) => {
+    const { jwt } = context;
+
+    const update = await fastify.inject({
+      method: "PUT",
+      url: "/v2/users/me",
+      headers: {
+        authorization: `Bearer ${jwt}`,
+      },
+      payload: { favorites: [{ id: 1 }] },
+    });
+    t.equal(update.statusCode, 200);
+
+    const profile = await fastify.inject({
+      url: "/v2/users/me",
+      headers: {
+        authorization: `Bearer ${jwt}`,
+      },
+    });
+
+    t.equal(profile.statusCode, 200);
+    t.equal(profile.json().favorites.length, 1);
+  });
+});
