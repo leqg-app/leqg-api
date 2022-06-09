09fea2ca787c83cd701c8feeefd38566189f6551
Nicolas de Chevigné <nico2che@gmail.com>
2022-06-10 01:16:13 +0300

    Remove validate store route on v1

--- src/v1/handlers/stores.js
+++ src/v1/handlers/stores.js
@@ -3,14 +3,10 @@ const S = require("fluent-json-schema");
 const { Store } = require("../../entity/Store.js");
 const { StoreRevision } = require("../../entity/StoreRevision.js");
 const { Version } = require("../../entity/Version.js");
-const { Contribution } = require("../../entity/Contribution.js");
-const { StoreValidation } = require("../../entity/StoreValidation.js");
 const { isRole, ROLES } = require("../../plugins/authentication.js");
 const { formatStore, formatStores } = require("../utils/format.js");
 const diffMapper = require("../utils/diffMapper.js");
-const { getCoordinatesDistance } = require("../utils/coordinates.js");
 const { calculateReputation } = require("../utils/reputation.js");
-const REPUTATIONS = require("../../reputations.js");
 
 const STORE_REVISION_FIELDS = [
   "name",
@@ -236,112 +232,9 @@ const updateStore = {
   },
 };
 
-const MAX_VALIDATION_DISTANCE = 0.04; // 40 meters
-const MIN_VALIDATION_TIME = 15 * 60 * 1000; // 15 minutes
-
-const validateStore = {
-  schema: {
-    summary: "User validate store",
-    tags: ["store"],
-    params: S.object().prop("id", S.integer().required()),
-    body: S.object()
-      .prop("longitude", S.number().required())
-      .prop("latitude", S.number().required()),
-    response: {
-      200: S.object().prop("reputation", S.integer()),
-      404: S.object().prop("error", S.string()),
-      422: S.object().prop("error", S.string()),
-      429: S.object().prop("error", S.string()),
-    },
-  },
-  onRequest: [isRole(ROLES.USER)],
-  handler: async (req, reply) => {
-    const { id } = req.params;
-    const { longitude, latitude } = req.body;
-
-    const repoStore = req.server.db.getRepository(Store);
-    const store = await repoStore.findOne({
-      where: { id },
-      relations: {
-        revisions: {
-          user: true,
-          contribution: true,
-        },
-        validations: {
-          user: true,
-        },
-      },
-    });
-
-    if (!store) {
-      return reply.status(404).send({ error: "store.notfound" });
-    }
-
-    const distance = getCoordinatesDistance(store, req.body);
-    if (distance > MAX_VALIDATION_DISTANCE) {
-      return reply.status(422).send({ error: "store.validation.position" });
-    }
-
-    const alreadyValidated = (store.validations || []).some(
-      ({ user }) => user.id === req.user.id
-    );
-    if (alreadyValidated) {
-      return reply.status(422).send({ error: "store.validation.already" });
-    }
-
-    const recentValidation = req.user.contributions.some(
-      ({ reason, createdAt }) =>
-        reason === "store.validation.creation" &&
-        createdAt > Date.now() - MIN_VALIDATION_TIME
-    );
-    if (recentValidation) {
-      return reply.status(429).send({ error: "store.validation.ratelimit" });
-    }
-
-    // Reward all contributors
-    const contributors = store.revisions.reduce((contributors, revision) => {
-      if (revision.user.id === req.user.id) {
-        return contributors;
-      }
-      if (!contributors[revision.user.id]) {
-        contributors[revision.user.id] = 0;
-      }
-      contributors[revision.user.id] += revision.contribution.reputation;
-      return contributors;
-    }, {});
-
-    await Promise.all(
-      Object.entries(contributors).map(([user, reputation]) => {
-        return req.server.db.manager.save(Contribution, {
-          user,
-          reputation: Math.floor(reputation / 4) || 1,
-          reason: "store.validation.reward",
-        });
-      })
-    );
-
-    // Save validation and reward user
-    const repoValidation = req.server.db.getRepository(StoreValidation);
-    await repoValidation.save({
-      longitude,
-      latitude,
-      user: req.user.id,
-      contribution: {
-        user: req.user.id,
-        reputation: REPUTATIONS.STORE.VALIDATION,
-        reason: "store.validation.creation",
-      },
-      store: store.id,
-    });
-
-    return { reputation: REPUTATIONS.STORE.VALIDATION };
-  },
-};
-
 module.exports = {
   getAllStores,
   getStore,
   createStore,
   updateStore,
-  validateStore,
 };
