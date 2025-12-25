c80ebd2bb35d8f3e780e0b44025af1986404899f
Nicolas de Chevigné <nico2che@gmail.com>
2025-12-25 23:58:35 +0100

    Log initialize db error

--- src/plugins/database.js
+++ src/plugins/database.js
@@ -4,24 +4,29 @@ const { DataSource } = require("typeorm");
 const entities = require("../entity/index.js");
 
 async function db(fastify) {
-  const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = fastify.config;
-  const AppDataSource = new DataSource({
-    type: "mysql",
-    host: DB_HOST,
-    port: DB_PORT,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    synchronize: true,
-    entities,
-  });
+  try {
+    const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = fastify.config;
+    const AppDataSource = new DataSource({
+      type: "mysql",
+      host: DB_HOST,
+      port: DB_PORT,
+      username: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME,
+      synchronize: true,
+      entities,
+    });
 
-  await AppDataSource.initialize();
+    await AppDataSource.initialize();
 
-  fastify.decorate("db", AppDataSource);
-  fastify.addHook("onClose", (fastifyInstance, done) => {
-    AppDataSource.destroy().then(done).catch(done);
-  });
+    fastify.decorate("db", AppDataSource);
+    fastify.addHook("onClose", (fastifyInstance, done) => {
+      AppDataSource.destroy().then(done).catch(done);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
 
 module.exports = fastifyPlugin(db);
