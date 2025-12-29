aefb62896346cf45517146f988bcd3f00e79c3cd
Nicolas de Chevigné <nico2che@gmail.com>
2025-12-29 20:09:19 +0100

    Don't synchronize db in prod

--- src/plugins/database.js
+++ src/plugins/database.js
@@ -13,7 +13,7 @@ async function db(fastify) {
       username: DB_USER,
       password: DB_PASSWORD,
       database: DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== "production",
       entities,
     });
 
